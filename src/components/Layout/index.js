63a38dfa2d76107d3ef77d9dc13a623ae7bf724d
carlos alberto triana brigante <brigantetca@gmail.com>
2023-02-10 11:08:20 -0500

    Sidebar with links and social media

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,7 +5,14 @@ import Sidebar from '../Sidebar'
 const Layout = () =>{
     return(
         <>
+        <div className='App'>
         <Sidebar/>
+            <div className='page'>
+                <span className='tags top-tags'> &lt;body&gt;</span>
+            </div>
+        </div>
+
+       
         </>
     )
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,22 +4,39 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () =>(
-<div className='nav-bar'>
-    <Link className='logo' to='/'>
-        <img src={LogoS} alt='logo' />
-        <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
-    </Link>
-    <NavLink exact='true' activeclassname='active' to='/'>
-        <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
-    </NavLink>
-    <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
-        <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
-    </NavLink>
-    <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
-        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
-    </NavLink>
+<div className="nav-bar">
+    <nav>
+        <Link className="logo" to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+        </NavLink>
+    </nav>
+    <ul>
+        <li>
+           <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/carlos-triana-77717a139/'>
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+
+            </a> 
+            <a target="_blank" rel="noreferrer" href='https://github.com/carlostrianab'>
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+
+            </a> 
+        </li>
+    </ul>
+    
+
 
 </div>
 )
