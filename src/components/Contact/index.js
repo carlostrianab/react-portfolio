c3a147ff30b5c6153a257d492f16127920dd7a66
carlos alberto triana brigante <brigantetca@gmail.com>
2023-02-28 17:30:17 -0500

    Form functionality enabled using emailJS

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,36 @@
+import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+
+    const refForm = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_lx2474t',
+                'template_yhe0tpx',
+                refForm.current,
+                'g3BOsN7rrCAc54cf6'
+            )
+            .then(
+                () => {
+                    alert('Message succesfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message')
+                }
+            )
+    }
 
     /*useEffect(() => {
         return setTimeout(() => {
@@ -26,21 +51,23 @@ const Contact = () => {
                         Paragraph 1 
                     </p>
                     <div className='contact-form'>
-                        <li className='half'>
-                            <input type='text' name='name' placeholder='Name' required/>
-                        </li>
-                        <li className='half'>
-                            <input type='email' name='email' placeholder='Email' required/>
-                        </li>
-                        <li>
-                            <input type='text' name='subject' placeholder='Subject' required/>
-                        </li>
-                        <li>
-                            <textarea  name='message' placeholder='Message' required/>
-                        </li>
-                        <li>
-                            <input type='submit' className='flat-button' value='SEND'/>
-                        </li>
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <li className='half'>
+                                <input type='text' name='name' placeholder='Name' required/>
+                            </li>
+                            <li className='half'>
+                                <input type='email' name='email' placeholder='Email' required/>
+                            </li>
+                            <li>
+                                <input type='text' name='subject' placeholder='Subject' required/>
+                            </li>
+                            <li>
+                                <textarea  name='message' placeholder='Message' required/>
+                            </li>
+                            <li>
+                                <input type='submit' className='flat-button' value='SEND'/>
+                            </li>
+                        </form>
                     </div>
                 </div>
             </div>
