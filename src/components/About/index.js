b532d44c7115156c90b907e272a0d39a30fb37b8
carlos alberto triana brigante <brigantetca@gmail.com>
2023-02-28 11:07:21 -0500

    cubespinner animation in About page completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,19 @@
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    /*useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, []);*/
+
+    
     return(
         <div className='container about-page'>
             <div className='text-zone'>
@@ -22,7 +34,28 @@ const About = () => {
                 </p>
 
             </div>
-
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faNodeJs} color='#68a063' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
