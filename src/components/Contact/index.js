620a10bf43fd731ff641f64120cf351ad9f7b869
carlos alberto triana brigante <brigantetca@gmail.com>
2023-03-01 16:47:50 -0500

    Leaflet map API added to the Contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,7 @@ import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Popup, TileLayer, Marker} from 'react-leaflet';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -73,6 +74,26 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Carlos Triana,
+                    <br/>
+                    Bogota, Colombia 
+                    <br/>
+                    <span>brigantetca@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman'/>
