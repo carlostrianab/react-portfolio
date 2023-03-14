3cd2e810eed98699cfd4a6400dddc55d5b577787
carlos alberto triana brigante <brigantetca@gmail.com>
2023-03-14 13:52:34 -0500

    Home page main info re-styled

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 
 import LogoTitle from '../../assets/images/c-logo.png'
+import Photo from '../../assets/images/photo.JPG'
 import {Link} from 'react-router-dom'
 import './index.scss'
 import { useEffect, useState } from 'react';
@@ -10,7 +11,7 @@ import Loader from 'react-loaders';
 const Home = () => {
     const[letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['a','r','l','o','s',' ', 'T','r','i','a','n','a']
-    const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r','.']
+    const jobArray = ['S','o','f','t','w','a','r','e',' ', 'D','e','v','e','l','o','p','e','r','.']
 
     //PENDING IMPLEMENTATION
     /*useEffect(() => {
@@ -23,22 +24,38 @@ const Home = () => {
         <>
             <div className='container home-page'>
             <div className='text-zone'>
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
+                <div className='top-lettering'>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <span> </span>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                </div>
                 <br/>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt='developer'/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}/>
+                <div className='animated-name'>
+                    <h1>
+                        <img src={LogoTitle} alt='developer'/>
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15}/>
+                    </h1>
+                </div>
                 <br/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22}/>
-                </h1>
-                <h2> Frontend Developer / JavaScript  / Python / ReactJS </h2>
+                <div className='image-section'>
+                    <img src={Photo} className='photo'/>
+                </div>
+               
+                <div className='animated-job'>
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={10}/>
+                </div>
+                <br/>
+                <div className='stack-info'>
+                    Frontend Developer / JavaScript  / Python / ReactJS
+                </div>
+               
+                
                 <Link to='/Contact' className='flat-button'>CONTACT ME </Link>
             </div>
             <Logo/>
