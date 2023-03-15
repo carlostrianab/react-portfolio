43d60c1512ef817bd95469053455fccb5414dd28
carlos alberto triana brigante <brigantetca@gmail.com>
2023-03-15 17:24:48 -0500

    Vertical Sidebar disapears in screens < 576px

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,37 +5,41 @@ import LogoSubtitle from '../../assets/images/carlos-sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { Container} from 'react-bootstrap'
+
 
 const Sidebar = () =>(
-<div className="nav-bar">
-    
-        <Link className="logo" to='/'>
-            <img src={cLogo} alt="logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-        </Link>
-    <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#FFFFFF"/>
-        </NavLink>
-        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-            <FontAwesomeIcon icon={faUser} color='#FFFFFF'/>
-        </NavLink>
-        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-            <FontAwesomeIcon icon={faEnvelope} color='#FFFFFF'/>
-        </NavLink>
-    </nav>
-    <ul>
-        <li>
-           <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/carlosalbertotrianabrigante-frontenddeveloper/'>
-                <FontAwesomeIcon icon={faLinkedin} color="#FFFFFF"/>
+<div className='show-for-small-only'>
+    <div className="nav-bar">
+                <Link className="logo" to='/'>
+                                <img src={cLogo} alt="logo" />
+                                <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+                            </Link>
+                        <nav>
+                            <NavLink exact="true" activeclassname="active" to="/">
+                                <FontAwesomeIcon icon={faHome} color="#FFFFFF"/>
+                            </NavLink>
+                            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                                <FontAwesomeIcon icon={faUser} color='#FFFFFF'/>
+                            </NavLink>
+                            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                                <FontAwesomeIcon icon={faEnvelope} color='#FFFFFF'/>
+                            </NavLink>
+                        </nav>
+                        <ul>
+                            <li>
+                            <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/carlosalbertotrianabrigante-frontenddeveloper/'>
+                                    <FontAwesomeIcon icon={faLinkedin} color="#FFFFFF"/>
 
-            </a> 
-            <a target="_blank" rel="noreferrer" href='https://github.com/carlostrianab'>
-                <FontAwesomeIcon icon={faGithub} color="#FFFFFF"/>
-            </a> 
-        </li>
-    </ul>
+                                </a> 
+                                <a target="_blank" rel="noreferrer" href='https://github.com/carlostrianab'>
+                                    <FontAwesomeIcon icon={faGithub} color="#FFFFFF"/>
+                                </a> 
+                            </li>
+                    </ul>
+        </div>
 </div>
+    
 )
 
 export default Sidebar
